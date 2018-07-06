98732197065307957998ad9a7226e9666f145037
Samuel Kwok <samuel.kwok@ntrust.com>
2018-07-06 12:00:58 -0700

    trigger build

--- app.js
+++ app.js
@@ -3,8 +3,8 @@ var express = require('express');
 var app = express();
  
 app.get('/', function (req, res) {
-  //res.send('dev testing environment is up and running!!!');
-  res.send('hello world');
+  res.send('dev testing environment is up and running!!!');
+  //res.send('hello world');
 });
  
 app.listen(process.env.PORT || 5000);
