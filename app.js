ac8a89bb49d67f63acb5b4b41161bbfc55599bdd
Samuel Kwok <samuel.kwok@ntrust.com>
2018-07-06 12:08:37 -0700

    trigger build 2

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 var express = require('express');
  
 var app = express();
- 
+// added more steps after setup completed 
 app.get('/', function (req, res) {
   res.send('dev testing environment is up and running!!!');
   //res.send('hello world');
